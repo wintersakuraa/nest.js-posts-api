e134ace52dc1769743fa8f9322c0dcdc792f70cf
WinterSakuraa <darwinwoda@gmail.com>
2022-11-04 22:32:57 +0200

    plainToClass -> plainToInstance

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,12 +1,12 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { ValidationException } from '../exceptions';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
     async transform(value: any, metadata: ArgumentMetadata) {
-        const obj = plainToClass(metadata.metatype, value);
+        const obj = plainToInstance(metadata.metatype, value);
         const errors = await validate(obj);
 
         if (errors.length) {
